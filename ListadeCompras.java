194db12506044e8270508d8c85620242e537e9c0
Maicon Henrique Wagner <wagner.maiconhenrique@gmail.com>
2026-04-22 15:10:40 -0300

    Iniciado projeto de lista de compras em Java

---
+++ ListadeCompras.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.contrel.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+
+
+public class ListadeCompras extends Application{
+
+    private ArrayList<String> listaDeCompras = new ArrayList<>();
+    private ListView<String> listaDeVisualizacao = new ListView<>();
+
+    @Override
+    public void start(Stage palco){
+
+    }
+    
+}
